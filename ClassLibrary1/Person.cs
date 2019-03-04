c3346fcb0a62d4a349d27e5ebeaa1c90b94950bc
Владислав Труфанов <@local.itstep>
2019-03-04 17:55:12 +0600

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string fname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person()
+        {
+            this.name = name;
+            this.fname = fname;
+            this.DateOfBirth = DateOfBirth;
+        }
+    }
+}
---
+++ ClassLibrary1/PersonSt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class PersonSt
+    {
+        public static string PersonInfo(Person p)
+        {
+            return String.Format("name - {0}, fname - {1}, DateOfBirth - {2}", p.name, p.fname, p.DateOfBirth);
+        }
+    }
+}
